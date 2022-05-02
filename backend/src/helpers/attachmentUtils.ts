0068982712daecaff91ded1b649786cb582c34b2
thanhbao0390 <46698469+thanhbao0390@users.noreply.github.com>
2022-05-02 22:46:25 +0700

    add

---
+++ backend/src/helpers/attachmentUtils.ts
@@ -0,0 +1,19 @@
+import * as AWS from 'aws-sdk'
+import * as AWSXRay from 'aws-xray-sdk'
+
+const XAWS = AWSXRay.captureAWS(AWS)
+const s3 = new XAWS.S3({
+    signatureVersion: 'v4'
+  })
+  
+// ##TODO: Implement the fileStogare logic
+const bucketName = process.env.ATTACHMENT_S3_BUCKET
+const urlExpiration = process.env.SIGNED_URL_EXPIRATION
+
+export async function createAttachmentPresignedUrl(todoId: string) {
+    return s3.getSignedUrl('putObject', {
+        Bucket: bucketName,
+        Key: todoId,
+        Expires: urlExpiration
+    })
+}
